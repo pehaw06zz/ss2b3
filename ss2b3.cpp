d9eaca328320c190ada0e78b413b627b644649bc
pehaw06zz <nhattran22447@gmail.com>
2024-12-10 15:56:44 +0700

    Add files via upload

---
+++ ss2b3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+	int n1 = 10	;
+	int n2 = 5 ;
+	// tinh tong hai bien
+	int sum = n1 + n2;
+	int difference = n1-n2;
+	int product = n1 * n2 ;
+	int quotient = n1 / n2 ; 
+	// in ket qua 
+	printf("tong cua %d va %d la: %d\n",n1,n2,sum);
+	printf("tong cua %d va %d la: %d\n",n1,n2,difference);
+	printf("tong cua %d va %d la: %d\n",n1,n2,product);
+	printf("tong cua %d va %d la: %d\n",n1,n2,quotient);
+	
+	return 0;
+	
+}
